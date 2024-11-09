a20c9fc3ca96df9c767e3fdbc29fb3739059bfd3
Renatotix <fatito124@hotmail.com>
2024-11-09 11:52:55 -0300

    Primer commit

---
+++ tasks/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
+# Create your views here.
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {"form": UserCreationForm})
+    else:
+
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(
+                    request.POST["username"], password=request.POST["password1"])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except:
+                return render(request, 'signup.html', {"form": UserCreationForm, "error": "Username already exists."})
+
+        return render(request, 'signup.html', {"form": UserCreationForm, "error": "Passwords did not match."})
+
+
+def home(request):
+    return render(request, 'Home.html')
+
+
+def tasks(request):
+    return render(request, 'Tasks.html')
